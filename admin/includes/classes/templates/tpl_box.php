61e6e1592432bc386860b0d9f5e35b2191cff666
ecartz <github@ecartz.biz>
2019-12-01 21:20:00 -0500

    Create tpl_box.php

---
+++ admin/includes/classes/templates/tpl_box.php
@@ -0,0 +1,15 @@
+<div class="card infobox">
+  <span class="card-header infoBoxHeading"><?php echo $heading ?></span>
+  <div class="card-body"><?php echo $contents ?></div>
+</div>
+<?php
+/*
+  $Id$
+
+  osCommerce, Open Source E-Commerce Solutions
+  http://www.oscommerce.com
+
+  Copyright (c) 2019 osCommerce
+
+  Released under the GNU General Public License
+*/
