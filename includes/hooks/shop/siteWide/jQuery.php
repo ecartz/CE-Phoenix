70c72f0f56f4549b7939674c27ce33da3afba8ee
ecartz <github@ecartz.biz>
2019-12-12 01:49:58 -0500

    Update jQuery.php

--- includes/hooks/shop/siteWide/jQuery.php
+++ includes/hooks/shop/siteWide/jQuery.php
@@ -15,11 +15,12 @@
 */
 
 class hook_shop_siteWide_jQuery {
-  var $version = '3.4.1';
-  
-  var $afterfooter = null;
 
-  function listen_injectAfterFooter() {
+  public $version = '3.4.1';
+  
+  public $afterfooter = null;
+
+  public function listen_injectAfterFooter() {
     $this->afterfooter .= '<!-- jquery hooked -->' . PHP_EOL;
     $this->afterfooter .= '<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.min.js" integrity="sha384-vk5WoKIaW/vJyUAd9n/wmopsmNhiy+L2Z+SBxGYnUkunIxVxAv/UtMOhba/xskxh" crossorigin="anonymous"></script>' . PHP_EOL;
 
