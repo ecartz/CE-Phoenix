59542f2511c12aca085f2412b5f12225b287f470
ecartz <github@ecartz.biz>
2019-12-12 00:23:57 -0500

    Cloudflare
    Use CDNJS Cloudflare to serve Bootstrap to minimize DNS queries.

--- includes/hooks/shop/siteWide/bootStrap.php
+++ includes/hooks/shop/siteWide/bootStrap.php
@@ -22,7 +22,7 @@ class hook_shop_siteWide_bootStrap {
   
   function listen_injectSiteStart() {
     $this->sitestart .= '<!-- bs hooked -->' . PHP_EOL;
-    $this->sitestart .= '<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">' . PHP_EOL;
+    $this->sitestart .= '<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.3.1/css/bootstrap.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">' . PHP_EOL;
 
     return $this->sitestart;
   }
@@ -30,7 +30,7 @@ class hook_shop_siteWide_bootStrap {
   function listen_injectSiteEnd() {
     $this->siteend .= '<!-- bs hooked -->' . PHP_EOL;
     $this->siteend .= '<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>' . PHP_EOL;
-    $this->siteend .= '<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>' . PHP_EOL;
+    $this->siteend .= '<script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>' . PHP_EOL;
 
     return $this->siteend;
   }
