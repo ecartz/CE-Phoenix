857a259c103f03fba1e0e7f55d16a39f629cb6dd
Matt Fletcher <github@ecartz.biz>
2020-12-18 17:11:03 +0000

    Simplify category depth calculation

--- index.php
+++ index.php
@@ -13,21 +13,10 @@
   require 'includes/application_top.php';
 
 // the following cPath references come from application_top.php
-  $category_depth = 'top';
   if (isset($cPath) && tep_not_null($cPath)) {
-    $categories_products_query = tep_db_query("SELECT COUNT(*) AS total FROM products_to_categories WHERE categories_id = " . (int)$current_category_id);
-    $categories_products = tep_db_fetch_array($categories_products_query);
-    if ($categories_products['total'] > 0) {
-      $category_depth = 'products'; // display products
-    } else {
-      $category_parent_query = tep_db_query("SELECT COUNT(*) AS total FROM categories WHERE parent_id = " . (int)$current_category_id);
-      $category_parent = tep_db_fetch_array($category_parent_query);
-      if ($category_parent['total'] > 0) {
-        $category_depth = 'nested'; // navigate through the categories
-      } else {
-        $category_depth = 'products'; // category has no products, but display the 'no products' message
-      }
-    }
+    $category_depth = (count($category_tree->get_children($current_category_id)) > 0) ? 'nested' : 'products';
+  } else {
+    $category_depth = 'top';
   }
 
   require "includes/languages/$language/index.php";
